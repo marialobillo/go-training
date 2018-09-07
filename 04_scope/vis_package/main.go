4ff51ff92d690187410140834005eb5882766e65
marialobillo <maria.lobillo.santos@gmail.com>
2018-09-07 10:11:10 +0200

    scope

---
+++ 04_scope/vis_package/main.go
@@ -0,0 +1,8 @@
+package vis
+
+import "fmt"
+
+funct PrintVar(){
+  fmt.Println(MyName)
+  fmt.Println(yourName)
+}
---
+++ 04_scope/vis_package/printer.go
@@ -0,0 +1,4 @@
+package vis
+
+var MyName = "Maria"
+var yourName = "Future Programmer"
